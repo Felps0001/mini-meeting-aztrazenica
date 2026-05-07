18c5a7b69066daaec4135eaa865697e011f78e84
Fellipe Fontes <fellipe.fontes@grifa.digital>
2026-05-07 11:41:29 -0300

    fix: update base path and homepage to new repo name astrazeneca

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === 'production' ? '/mini-meeting-aztrazeneca/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/mini-meeting-astrazeneca/' : '/',
   optimizeDeps: {
     include: ['react-signature-canvas']
   },
