c074e12719b405b3f76de04b3a930c5b6f506bfa
Fellipe Fontes <fellipe.fontes@grifa.digital>
2026-05-11 11:33:41 -0300

    fix: set base path to / for Cloudflare Pages

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === 'production' ? '/mini-meeting-astrazeneca/' : '/',
+  base: '/',
   optimizeDeps: {
     include: ['react-signature-canvas']
   },
